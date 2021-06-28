3a33160a4a899530a81c1bb726df61d8189f07f3
Serkan Berk Bilgiç <serkanberk@Serkan-MacBook-Pro.local>
2021-06-28 10:02:40 +0300

    first commit

---
+++ Pratikler/console-programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string s = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi, "+ s);
+        }
+    }
+}
