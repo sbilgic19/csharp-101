fbbdd1921886b68acd7987ac26adc1cf7ed332fe
Serkan Berk Bilgiç <serkanberk@Serkan-MacBook-Pro.local>
2021-06-29 01:48:32 +0300

    new file added

--- Pratikler/while-loop/Program.cs
+++ Pratikler/while-loop/Program.cs
@@ -6,7 +6,31 @@ namespace while_loop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lütfen bir sayı giriniz. ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi){
+                toplam += sayac;
+                sayac ++;
+            }
+
+            Console.WriteLine("Ortalama: "+toplam/sayi);
+
+            char character = 'a';
+            while(character < 'z'){
+                Console.WriteLine(character);
+                character ++;
+            }
+
+            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+            foreach(var item in arabalar)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
         }
     }
 }
