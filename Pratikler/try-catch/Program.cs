20ed42c11179a1520f864db467ea0b2fbfe26476
Serkan Berk Bilgiç <serkanberk@Serkan-MacBook-Pro.local>
2021-06-28 23:18:29 +0300

    updated

--- Pratikler/try-catch/Program.cs
+++ Pratikler/try-catch/Program.cs
@@ -6,10 +6,55 @@ namespace try_catch
     {
         static void Main(string[] args)
         {
+            /*
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi: "+sayi);
 
-            Console.WriteLine("HELLO!!!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata: "+ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı. ");
+            }
             
+            */
+
+            try
+            {
+                int a = int.Parse(null);
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş bir değer girdiniz. ");
+                Console.WriteLine(ex);
+            }
             
+
+            try
+            {
+                // int x = int.Parse("test");
+                int b = int.Parse("-20000000000");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil. ");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz. ");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı. ");
+            }
             
 
             
