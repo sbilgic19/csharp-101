81621442e0ad28c9e177323fd8d680fed50b47ea
Serkan Berk Bilgiç <serkanberk@Serkan-MacBook-Pro.local>
2021-06-29 08:43:53 +0300

    array-methods added

---
+++ Pratikler/array-methods/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace array_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] sayiDizisi = {23,12,86,11,3,17,72,37};
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Array.Sort(sayiDizisi);
+            Console.WriteLine("Sıralı Sayı Dizisi");
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            // Clear
+
+            Console.WriteLine("*** Array Clear ***");
+
+            Array.Clear(sayiDizisi,2,2); // 2.indeksten başlayarak 2 tane elemanı 0'lar.
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+
+            // Reverse
+
+            Console.WriteLine("*** Array Reverse ***");
+            Array.Reverse(sayiDizisi);
+
+
+            // IndexOf
+
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+
+            // Resize
+
+            Array.Resize<int>(ref sayiDizisi, 11);
+
+            sayiDizisi[9] = 99;
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+
+
+
+            
+            
+
+
+
+        }
+    }
+}
---
+++ Pratikler/hackerrank-solutions/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace hackerrank_solutions
+{
+class Result
+{
+
+    
+
+    public static void staircase(int n)
+    {
+        for (int i=1; i<=n; i++){
+           for (int k=0; k < n-i; k++){
+               Console.Write(" ");
+           }
+           for( int j= 0; j < i; j++){
+               Console.Write("#");
+           }
+           Console.Write("\n");
+        }
+    }
+
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter staircase level: ");
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.staircase(n);
+        }
+
+        /*
+
+        -> Plus Minus Challenge Solution
+
+        public static void plusMinus(List<int> arr)
+    {
+        int totalNegative = 0;
+        int totalPositive = 0;
+        int totalZeros = 0;
+        
+        foreach (var num in arr)
+        {
+            if (num == 0)
+            {
+                totalZeros++;
+            }else if (num > 0){
+                totalPositive++;
+            }
+            else{
+                totalNegative++;
+            }
+        }
+        
+        Console.WriteLine(string.Format("{0:F6}", (decimal)totalPositive/arr.Count));
+        Console.WriteLine(string.Format("{0:F6}", (decimal)totalNegative/arr.Count));
+        Console.WriteLine(string.Format("{0:F6}", (decimal)totalZeros/arr.Count));
+        
+    }
+        */
+    }
+
+
+
+    
+}
