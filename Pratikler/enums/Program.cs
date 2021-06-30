d68feb8ca13bb5ef4f9441f1e58c554c7a1314dd
Serkan Berk Bilgiç <serkanberk@Serkan-MacBook-Pro.local>
2021-06-30 17:44:37 +0300

    enums and structs added

---
+++ Pratikler/enums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+            
+            int sicaklik = 32;
+
+            if(sicaklik <= (int)HavaDurumu.Normal){
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim. ");
+            }else if(sicaklik >= (int)HavaDurumu.Sicak){
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün ");
+
+            }else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSicak){
+                Console.WriteLine("Hadi dışarı çıkalım. ");
+            }
+
+
+
+
+
+
+
+        }
+
+        enum Gunler
+        {
+            Pazartesi = 1, // normalde 0 dan başlıyor.
+            Sali,
+            Carsamba,
+            Persembe,
+            Cuma=23,
+            Cumartesi,
+            Pazar
+        }
+
+        enum HavaDurumu
+        {
+            Soguk = 5,
+            Normal = 20,
+            Sicak = 25,
+            CokSicak = 30
+        }
+    }
+}
---
+++ Pratikler/structs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen d1 = new Dikdortgen();
+            d1.kisaKenar = 3;
+            d1.uzunKenar = 5;
+
+            Console.WriteLine("Class Alan Hesapla: {0} ",d1.AlanHesapla());
+
+
+            Console.WriteLine("***** Struct *****");
+            Dikdortgen_Struct d2 = new Dikdortgen_Struct();
+            // d2.kisaKenar = 3;
+            // d2.uzunKenar = 5;
+            Console.WriteLine("Class Alan Hesapla: {0} ",d2.AlanHesapla());
+
+            
+
+
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+
+        public Dikdortgen(){
+            kisaKenar = 3;
+            uzunKenar = 5;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar){
+            this.kisaKenar = kisaKenar;
+            this.uzunKenar = uzunKenar;
+        }
+    }
+}
